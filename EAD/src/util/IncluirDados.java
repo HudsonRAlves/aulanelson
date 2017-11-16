99ad34b530a976a86f9bd7f2a4aa9663f17196c5
hudsonalves <hudsonalves@localhost>
2017-11-16 21:01:11 -0200

    Exclusão das Tabelas Desnecessarias

--- EAD/src/util/IncluirDados.java
+++ EAD/src/util/IncluirDados.java
@@ -7,17 +7,9 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 
 import DAO.ClienteDAO;
-import DAO.ComentarioDAO;
-import DAO.FilmeDAO;
-import DAO.LocacaoDAO;
-import DAO.UnidadeDAO;
-import DAO.VHSDAO;
+
 import entidade.Cliente;
-import entidade.Comentario;
-import entidade.Filme;
-import entidade.Locacao;
-import entidade.Unidade;
-import entidade.VHS;
+
 
 public class IncluirDados {
 
@@ -28,11 +20,6 @@ public class IncluirDados {
 		/*StringBuilder sb = new StringBuilder();
 		StringBuilder resultado = new StringBuilder();*/
 		ClienteDAO clienteDao = new ClienteDAO();
-		ComentarioDAO comentarioDao = new ComentarioDAO();
-		FilmeDAO filmeDao = new FilmeDAO();
-		LocacaoDAO locacaoDao = new LocacaoDAO();
-		UnidadeDAO unidadeDao = new UnidadeDAO();
-		VHSDAO vshDao = new VHSDAO();
 		
 		do {
 
@@ -54,39 +41,6 @@ public class IncluirDados {
 				a.setCpf(JOptionPane.showInputDialog("Digite o CPF:"));
 				a.setNome(JOptionPane.showInputDialog("Digite o Nome:"));
 				clienteDao.incluir(a);
-				break;
-
-			case 2:
-				Comentario b = new Comentario();
-				b.setAvaliacao(Double.parseDouble(JOptionPane.showInputDialog("Digite sua Avaliação")));
-				b.setConteudo(JOptionPane.showInputDialog("Digite o Conteudo"));
-				comentarioDao.incluir(b);
-				break;
-			
-			case 3:
-				Filme c = new Filme();
-				c.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Digite o Código")));
-				c.setNome(JOptionPane.showInputDialog("Digite o Nome do Filme"));
-				filmeDao.incluir(c);
-				break;
-			
-			case 4:
-				Locacao d = new Locacao();
-				d.setDataLocacao((java.sql.Date) DataUtil.parseDate(JOptionPane.showInputDialog("Digite a Data de Locação")));
-				d.setDataDevolucao((java.sql.Date) DataUtil.parseDate(JOptionPane.showInputDialog("Digite a Data de Devolução")));
-				locacaoDao.incluir(d);
-				break;
-				
-			case 5: 
-				Unidade e = new Unidade();
-				e.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Digite o Código da Unidade")));
-				unidadeDao.incluir(e);
-				break;
-			
-			case 6:
-				VHS f = new VHS();
-				f.setDublado(Integer.parseInt(JOptionPane.showInputDialog("Digite a Data de Locação")));
-				vshDao.incluir(f);
 				break;
 			
 			default:
